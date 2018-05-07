e826b81f1df51994608d90d29dc46750af59f990
sdiksha09 <diksha.sharma1209@gmail.com>
2018-05-07 19:29:26 +0530

    commit

---
+++ app/src/main/java/com/example/shipra/cloudy_app/login.java
@@ -0,0 +1,13 @@
+KOTLINJAVA
+public class login extends signUp {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+    }
+}
---
+++ app/src/main/java/com/example/shipra/cloudy_app/signUp.java
@@ -0,0 +1,13 @@
+package com.example.shipra.cloudy_app;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class signUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
